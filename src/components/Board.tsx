d074b3adc7cbfd093702efdbf2eaa60ba7284de9
Petr Janik <peta.janik@email.cz>
2021-12-04 23:51:48 +0100

    Add speed slider

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,6 +2,21 @@ import { useState } from 'react';
 import getNextGeneration, { Coordinate, CoordinateSet } from '../getNextGeneration';
 import './Board.css';
 import classNames from 'classnames';
+import { Slider, Stack } from '@mui/material';
+import SpeedIcon from '@mui/icons-material/Speed';
+
+const GLIDER: Coordinate[] = [
+  [4, 4],
+  [5, 5],
+  [6, 3],
+  [6, 4],
+  [6, 5],
+];
+const PERIODIC: Coordinate[] = [
+  [5, 4],
+  [5, 5],
+  [5, 6],
+];
 
 const getCurrentGeneration = (generations: CoordinateSet[]) => {
   return generations[generations.length - 1];
@@ -11,16 +26,9 @@ const Board = () => {
   const [width, setWidth] = useState(10);
   const [height, setHeight] = useState(10);
   const [simulationClearInterval, setSimulationClearInterval] = useState<ReturnType<typeof setInterval>>();
+  const [simulationSpeed, setSimulationSpeed] = useState(50);
 
-  const [generations, setGenerations] = useState([
-    new CoordinateSet([
-      [4, 4],
-      [5, 5],
-      [6, 3],
-      [6, 4],
-      [6, 5],
-    ]),
-  ]);
+  const [generations, setGenerations] = useState([new CoordinateSet(PERIODIC)]);
 
   const getCellState = (coordinate: Coordinate) =>
     getCurrentGeneration(generations).has(coordinate) ? 'alive' : 'dead';
@@ -43,13 +51,22 @@ const Board = () => {
   };
 
   const runSimulation = () => {
-    setSimulationClearInterval(setInterval(() => stepForward(), 500));
+    setSimulationClearInterval(setInterval(() => stepForward(), 500000 / (simulationSpeed * simulationSpeed)));
   };
 
   const stopSimulation = () => {
     if (simulationClearInterval) {
       clearInterval(simulationClearInterval);
     }
+  };
+
+  const handleSimulationSpeedChange = (_: Event, newSpeed: number | number[]) => {
+    if (Array.isArray(newSpeed)) {
+      return;
+    }
+    setSimulationSpeed(newSpeed);
+    stopSimulation();
+    runSimulation();
   };
 
   return (
@@ -72,6 +89,10 @@ const Board = () => {
         <button onClick={increaseSize}>Increase size</button>
         <button onClick={runSimulation}>Run simulation</button>
         <button onClick={stopSimulation}>Stop simulation</button>
+        <Stack spacing={2} direction="column" sx={{ mb: 1 }} alignItems="center">
+          <SpeedIcon />
+          <Slider aria-label="Simulation speed" value={simulationSpeed} onChange={handleSimulationSpeedChange} />
+        </Stack>
       </div>
     </div>
   );
